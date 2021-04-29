0d1b6ec0ed2aa73f9fefc3dd59f7c694e5be8d3d
Filip Hodun <filiphodun@gmail.com>
2021-04-29 19:40:44 +0200

    initial commit

---
+++ CppCLR_WinformsProject/main.cpp
@@ -0,0 +1,12 @@
+#include "Form1.h";
+
+using namespace $safeprojectname$;
+
+[STAThread]
+int main(array<System::String^>^ args)
+{
+    Application::EnableVisualStyles();
+    Application::SetCompatibleTextRenderingDefault(false);
+    Application::Run(gcnew Form1());
+    return 0;
+}
